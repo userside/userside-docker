86cda1a97b1695510f996240ec375c8cc7cc902d
Denis Bondar <bondar.den@gmail.com>
2019-05-07 11:11:52 +0300

    Fix init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 cp -n docker-compose-example.yml docker-compose.yml
 cp -n Makefile-example Makefile
 
-@echo "Init comlete. Please, edit docker-compose.yml file, then run"
-@echo ""
-@echo "  make install"
-@echo ""
+echo "Init comlete. Please, edit docker-compose.yml file, then run"
+echo ""
+echo "  make install"
+echo ""
