ac5582eee30918b12e5857fc9741323ff2f6358a
Denis Bondar <bondar.den@gmail.com>
2019-05-08 10:18:55 +0300

    Rename docker-compose-example.yml to docker-compose.yml-example

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -n docker-compose-example.yml docker-compose.yml
+cp -n docker-compose.yml-example docker-compose.yml
 cp -n Makefile-example Makefile
 
 echo "Init comlete. Please, edit docker-compose.yml file, then run"
