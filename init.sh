1d835aba85ef5a1c5e54bbdf0826f31b98e13110
Denis Bondar <bondar.den@gmail.com>
2020-07-27 16:21:26 +0300

    Remove installer config

--- init.sh
+++ init.sh
@@ -2,7 +2,6 @@
 
 cp -n docker-compose.yml-example docker-compose.yml
 cp -n Makefile-example Makefile
-cp -n installer.json-example installer.json
 
 echo "Init comlete. Please, edit docker-compose.yml file, then run"
 echo ""
