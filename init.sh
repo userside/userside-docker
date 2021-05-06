09d315b5d6ade79ff3470a604f1f9b99fdd6c9b3
Denis Bondar <bondar.den@gmail.com>
2021-05-06 16:28:09 +0300

    First build of bundle 3.16

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 cp -n docker-compose.yml-example docker-compose.yml
 cp -n Makefile-example Makefile
+cp -n .env-example .env
 
 echo "Init comlete. Please, edit docker-compose.yml file, then run"
 echo ""
