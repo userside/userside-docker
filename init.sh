4feab207a8ed20ff06ea337d781b3b21185220ea
Denis Bondar <bondar.den@gmail.com>
2021-05-14 11:45:22 +0300

    Fix Update instructions and USERSIDE_BASE_DIR

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ cp -n docker-compose.yml-example docker-compose.yml
 cp -n Makefile-example Makefile
 cp -n .env-example .env
 
-echo "Init comlete. Please, edit docker-compose.yml file, then run"
+echo "Init comlete. Please, edit .env file, then run"
 echo ""
 echo "  make install"
 echo ""
