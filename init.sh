810caded7bba7b6e1be5f7aa49b8e69829c83a9d
Denis Bondar <bondar.den@gmail.com>
2022-07-07 10:24:55 +0300

    Приведение docker-compose.yml в соответствие с новыми требованиями

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-cp -n docker-compose.yml-example docker-compose.yml
+test -f docker-compose.yml && mv -n docker-compose.yml compose.yaml
+cp -n compose.yaml-example compose.yaml
 cp -n Makefile-example Makefile
 cp -n .env-example .env
 
