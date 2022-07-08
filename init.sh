a6ea4395c63c0791438eaaa75fb348ff53189515
Denis Bondar <bondar.den@gmail.com>
2022-07-08 13:20:16 +0300

    Внедрение bundle.bash на замену Makefile

--- init.sh
+++ init.sh
@@ -4,8 +4,10 @@ test -f docker-compose.yml && mv -n docker-compose.yml compose.yaml
 cp -n compose.yaml-example compose.yaml
 cp -n Makefile-example Makefile
 cp -n .env-example .env
+cp -n bundle.bash-example bundle.bash
 
 echo "Init comlete. Please, edit .env file, then run"
 echo ""
-echo "  sudo make install"
+echo "  . bundle.bash"
+echo "  bundle-install"
 echo ""
