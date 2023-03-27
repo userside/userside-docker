eb9229cefee1a812182649d3375c4ae71f8916d1
Denis Bondar <bondar.den@gmail.com>
2023-03-27 20:09:59 +0300

    Init version 3.18

--- init.sh
+++ init.sh
@@ -6,8 +6,8 @@ cp -n Makefile-example Makefile
 cp -n .env-example .env
 cp -n bundle.bash-example bundle.bash
 
-echo "Init comlete. Please, edit .env file, then run"
+echo "Init comlete. Please, edit .env file, then run:"
 echo ""
-echo "  . bundle.bash"
+echo "  source bundle.bash"
 echo "  bundle-install"
 echo ""
