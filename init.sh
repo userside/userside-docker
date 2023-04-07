d0d5b9e55c0b2df9bdbdd252fc254fc739228838
Denis Bondar <bondar.den@gmail.com>
2023-04-07 16:40:47 +0300

    Update instructions for bundle 3.18

--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@
 
 test -f docker-compose.yml && mv -n docker-compose.yml compose.yaml
 cp -n compose.yaml-example compose.yaml
-cp -n Makefile-example Makefile
 cp -n .env-example .env
 cp -n bundle.bash-example bundle.bash
+test -f Makefile && mv -n Makefile Makefile_legacy
 
 echo "Init comlete. Please, edit .env file, then run:"
 echo ""
